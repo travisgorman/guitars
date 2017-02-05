1cf573449b33e11c89af2eada243b2b55aa4e24e
Travis Gorman <thefarbetween@me.com>
2017-02-05 10:27:09 -0600

    guitar preview component

--- src/components/partials/GuitarPreview.js
+++ src/components/partials/GuitarPreview.js
@@ -1,1 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router'
+
+export default class GuitarPreview extends React.Component {
+
+	render() {
+
+		return (
+			<div className="guitar-preview">
+				<h3>
+					<Link to={"/guitar/" + this.props.id}>
+						{this.props.name} <small>{this.props.manufacturer}</small>
+					</Link>
+				</h3>
+				<img className="image-responsive" src={"img/" + this.props.image}/>
+			</div>
+		  
+		);
+	}
+	
+}
